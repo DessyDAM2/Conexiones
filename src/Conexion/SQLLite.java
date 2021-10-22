d9534744e136c732bab399470897228735b63d04
DessyDAM2 <200076@iessanalberto.com>
2021-10-22 12:51:47 +0200

    Conexiones AD

---
+++ src/Conexion/SQLLite.java
@@ -0,0 +1,14 @@
+package Conexion;
+
+import java.sql.Connection;
+
+public class SQLLite {
+        private static final String URL = "jdbc:C//Users//Alumno_Fp//Desktop//SQLLite//";
+
+    public static void Connection(){
+        Connection con = null;
+    }
+    public static void main(String[] args) {
+        //
+    }
+}
