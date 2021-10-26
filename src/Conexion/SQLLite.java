3f9e624591177241d7e416bdafd8251884ba3504
DessyDAM2 <200076@iessanalberto.com>
2021-10-26 09:38:54 +0200

    Conexiones AD

--- src/Conexion/SQLLite.java
+++ src/Conexion/SQLLite.java
@@ -1,14 +1,27 @@
 package Conexion;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SQLLite {
-        private static final String URL = "jdbc:C//Users//Alumno_Fp//Desktop//SQLLite//";
+        private static final String URL = "jdbc:sqlite:C:/Users/Alumno_Fp/Desktop/SQLLite/chinook.db";
 
-    public static void Connection(){
+    public static Connection conectar(){
         Connection con = null;
+        try {
+
+            con = DriverManager.getConnection(URL);
+            System.out.println("Conexion OK");
+
+        } catch (SQLException e) {
+
+            System.out.println("Subnormal");
+
+        }
+        return con;
     }
     public static void main(String[] args) {
-        //
+        Connection micon = conectar();
     }
 }
